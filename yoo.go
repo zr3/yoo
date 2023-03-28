609de586c3ebcda428540bd7e395791efac45e29
zr3 <zak.reynolds+github@zakjr.com>
2023-03-27 17:03:49 -0700

    refactor yoo.go to improve error handling and readability

--- yoo.go
+++ yoo.go
@@ -6,10 +6,11 @@ import (
 	"fmt"
 	"io/ioutil"
 	"log"
+	"net/url"
 	"os"
 	"time"
 
-	termutil "github.com/andrew-d/go-termutil"
+	"github.com/andrew-d/go-termutil"
 	"github.com/gookit/config/v2"
 	"github.com/gookit/config/v2/yamlv3"
 	openai "github.com/sashabaranov/go-openai"
@@ -27,65 +28,108 @@ type Secrets struct {
 }
 
 type Persona struct {
-	Name string `mapstructure:"name"`
-	// https://github.com/sashabaranov/go-openai/blob/2ebb265e715ad6fc6c1f755705dfbd02d44a9111/completion.go
+	Name  string `mapstructure:"name"`
 	Model string `mapstructure:"model"`
 }
 
-func main() {
-	// read config
-	home, err := os.UserHomeDir()
-	checkFatal(err, "yoo depends on the home directory for configuration, and no home dir was found.")
+const (
+	configFileFormat   = "%s/.config/yoo/config.yml"
+	promptFileFormat   = "%s/.yoo/%s.%s.md"
+	systemPromptFormat = "%s/.config/yoo/%s.txt"
+	defaultPersonaName = "archie"
+	defaultModel       = "gpt-4"
+	defaultTitleName   = "summer-slug"
+	defaultTitleModel  = "gpt-3.5-turbo"
+)
 
+func main() {
+	yooConfig, err := loadYooConfig()
+	check(err, "Error loading YooConfig", true)
+
+	selectedPersona, titlePersona := determinePersonas(yooConfig)
+	systemPrompt, systemFile, err := loadSystemPrompt(os.Getenv("HOME"), selectedPersona)
+	check(err, fmt.Sprintf("Could not read the configured system prompt '%s'", systemFile), true)
+
+	userPrompt := getUserPrompt()
+
+	if !config.Bool("quiet") {
+		fmt.Println("...")
+	}
+
+	client := openai.NewClient(yooConfig.Secrets.OpenAIKey)
+	promptResponse, err := createChatCompletion(client, selectedPersona.Model, systemPrompt, userPrompt)
+	check(err, "Could not complete request to openai", true)
+
+	fmt.Println(promptResponse)
+
+	if !config.Bool("no-log") {
+		home := os.Getenv("HOME")
+		logResponse(home, titlePersona, userPrompt, systemPrompt, promptResponse, client)
+	}
+}
+
+func loadYooConfig() (YooConfig, error) {
+	home, err := os.UserHomeDir()
+	if err != nil {
+		return YooConfig{}, fmt.Errorf("yoo depends on the home directory for configuration, and no home dir was found: %w", err)
+	}
+
+	configFilePath := fmt.Sprintf(configFileFormat, home)
 	config.AddDriver(yamlv3.Driver)
 	yooConfig := YooConfig{}
-	configFile := home + "/.config/yoo/config.yml"
-	err = config.LoadFiles(configFile)
-	checkFatal(err, "yoo depends on '"+configFile+"' and it was not found.")
+
+	err = config.LoadFiles(configFilePath)
+	if err != nil {
+		return YooConfig{}, fmt.Errorf("yoo depends on '%s', and it was not found: %w", configFilePath, err)
+	}
+
 	err = config.BindStruct("", &yooConfig)
-	checkFatal(err, "yoo had a problem parsing config from '"+configFile+"'")
+	if err != nil {
+		return YooConfig{}, fmt.Errorf("yoo had a problem parsing config from '%s': %w", configFilePath, err)
+	}
 
 	flags := []string{"persona", "prompt", "quiet:bool", "no-log:bool"}
 	err = config.LoadFlags(flags)
-	checkWarn(err, "there was an issue parsing flags, so they will be ignored.")
-
-	// default to archie gpt-4 mainPersona
-	mainPersona := Persona{
-		Name:  "archie",
-		Model: "gpt-4",
+	if err != nil {
+		check(err, "There was an issue parsing flags, so they will be ignored.", false)
 	}
 
-	// default title persona
-	titlePersona := Persona{
-		Name:  "summer-slug",
-		Model: "gpt-3.5-turbo",
+	return yooConfig, nil
+}
+
+func determinePersonas(yooConfig YooConfig) (selectedPersona, titlePersona Persona) {
+	selectedPersona = Persona{
+		Name:  defaultPersonaName,
+		Model: defaultModel,
 	}
 
-	// override with configured default persona, if exists
+	titlePersona = Persona{
+		Name:  defaultTitleName,
+		Model: defaultTitleModel,
+	}
+
 	for _, p := range yooConfig.Personas {
 		if p.Name == yooConfig.DefaultPersona {
-			mainPersona = p
+			selectedPersona = p
 		}
 		if p.Name == yooConfig.TitlePersona {
 			titlePersona = p
 		}
 	}
 
-	// override with flagged persona, if provided
 	personaFlag := config.String("persona")
 	if personaFlag != "" {
 		for _, p := range yooConfig.Personas {
 			if p.Name == personaFlag {
-				mainPersona = p
+				selectedPersona = p
 			}
 		}
 	}
 
-	// read the systemPrompt prompt
-	systemPrompt, systemFile, err := loadSystemPrompt(home, mainPersona)
-	checkFatal(err, "could not read the configured system prompt '"+systemFile+"'.")
+	return selectedPersona, titlePersona
+}
 
-	// start userPrompt with cli argument
+func getUserPrompt() string {
 	userPrompt := ""
 	if len(os.Args) == 2 {
 		userPrompt += os.Args[1]
@@ -93,7 +137,6 @@ func main() {
 		userPrompt += config.String("prompt")
 	}
 
-	// if stdin was provided, add that to prompt
 	if !termutil.Isatty(os.Stdin.Fd()) {
 		inreader := bufio.NewReader(os.Stdin)
 		pipedinput, err := ioutil.ReadAll(inreader)
@@ -102,55 +145,17 @@ func main() {
 			userPrompt += string(pipedinput)
 		}
 	}
+	return userPrompt
+}
 
-	// print something for ux
-	if !config.Bool("quiet") {
-		fmt.Println("...")
-	}
-
-	// set up an openai client
-	client := openai.NewClient(yooConfig.Secrets.OpenAIKey)
-
-	// get the main prompt response
-	promptResponse, err := createChatCompletion(client, mainPersona.Model, systemPrompt, userPrompt)
-	checkFatal(err, "could not complete request to openai")
-
-	// write response out to console
-	fmt.Println(promptResponse)
-
-	// write response out to log
-	if !config.Bool("no-log") {
-		mainContent := div("prompt") + userPrompt + div("response") + promptResponse + div("system") + systemPrompt
-		titleSystemPrompt, titleSystemPromptFile, err := loadSystemPrompt(home, titlePersona)
-		checkFatal(err, "could not read the configured title system prompt '"+titleSystemPromptFile+"'.")
-		title, err := createChatCompletion(client, titlePersona.Model, titleSystemPrompt, mainContent)
-		checkWarn(err, "could not complete request to openai for title slug")
-		if err != nil {
-			title = "unknown-topic"
+func check(err error, message string, fatal bool) {
+	if err != nil {
+		fmt.Println(message)
+		if fatal {
+			log.Fatal(err)
+		} else {
+			fmt.Println(err)
 		}
-		currentTime := time.Now().Local().Format("2006-01-02--15-04-05-MST")
-		logName := home + "/.yoo/" + currentTime + "." + title + ".md"
-		metaContent := "# " + title + "\n\n" + currentTime
-		content := metaContent + mainContent
-		os.WriteFile(logName, []byte(content), 0644)
-	}
-}
-
-func div(title string) string {
-	return "\n\n## " + title + "\n\n"
-}
-
-func checkFatal(err error, message string) {
-	if err != nil {
-		fmt.Println(message)
-		log.Fatal(err)
-	}
-}
-
-func checkWarn(err error, message string) {
-	if err != nil {
-		fmt.Println(message)
-		fmt.Println(err)
 	}
 }
 
@@ -178,7 +183,30 @@ func createChatCompletion(client *openai.Client, model string, system string, pr
 }
 
 func loadSystemPrompt(home string, persona Persona) (string, string, error) {
-	systemfile := home + "/.config/yoo/" + persona.Name + ".txt"
+	systemfile := fmt.Sprintf(systemPromptFormat, home, persona.Name)
 	systemcontent, err := os.ReadFile(systemfile)
 	return string(systemcontent), systemfile, err
+}
+
+func sectionDivider(title string) string {
+	sanitizedTitle := url.QueryEscape(title)
+	return fmt.Sprintf("\n\n## %s\n\n", sanitizedTitle)
+}
+
+func logResponse(home string, titlePersona Persona, userPrompt, systemPrompt, promptResponse string, client *openai.Client) {
+	mainContent := sectionDivider("prompt") + userPrompt + sectionDivider("response") + promptResponse + sectionDivider("system") + systemPrompt
+	titleSystemPrompt, titleSystemPromptFile, err := loadSystemPrompt(home, titlePersona)
+	check(err, fmt.Sprintf("Could not read the configured title system prompt '%s'", titleSystemPromptFile), false)
+
+	title, err := createChatCompletion(client, titlePersona.Model, titleSystemPrompt, mainContent)
+	check(err, "Could not complete request to openai for title slug", false)
+
+	if err != nil {
+		title = "unknown-topic"
+	}
+	currentTime := time.Now().Local().Format("2006-01-02--15-04-05-MST")
+	logName := fmt.Sprintf(promptFileFormat, home, currentTime, title)
+	metaContent := "# " + title + "\n\n" + currentTime
+	content := metaContent + mainContent
+	os.WriteFile(logName, []byte(content), 0644)
 }
