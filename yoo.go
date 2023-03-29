431421261b89f6e632d87a5c19646be7a1a4fff0
zr3 <zak.reynolds+github@zakjr.com>
2023-03-29 00:31:56 -0700

    refactor yoo.go to support multiple modes and personas, add chat, config, and show stubs

--- yoo.go
+++ yoo.go
@@ -32,39 +32,143 @@ type Persona struct {
 	Model string `mapstructure:"model"`
 }
 
+type Mode int
+
+const (
+	Normal Mode = iota
+	Chat
+	Config
+	Show
+)
+
+type LoadedPersona struct {
+	Name          string
+	Model         string
+	SystemMessage openai.ChatCompletionMessage
+}
+
+type LoadedResources struct {
+	OpenAIClient      *openai.Client
+	ChatPersona       LoadedPersona
+	TitlePersona      LoadedPersona
+	SelectedMode      Mode
+	InitialUserPrompt string
+	ConfigDir         string
+	LogDir            string
+}
+
 func main() {
 	// read config
+	loadedResources := loadConfig()
+
+	switch loadedResources.SelectedMode {
+	case Normal:
+		normalMode(loadedResources)
+	case Chat:
+		chatMode()
+	case Config:
+		configMode()
+	case Show:
+		showMode()
+	default:
+		log.Fatal("invalid mode '" + string(loadedResources.SelectedMode) + "'")
+	}
+}
+
+func normalMode(loadedResources LoadedResources) {
+	// if stdin was provided, add that to prompt
+	if !termutil.Isatty(os.Stdin.Fd()) {
+		inreader := bufio.NewReader(os.Stdin)
+		pipedinput, err := ioutil.ReadAll(inreader)
+		if err == nil {
+			loadedResources.InitialUserPrompt += "\n\n"
+			loadedResources.InitialUserPrompt += string(pipedinput)
+		}
+	}
+
+	// print something for ux
+	if !config.Bool("quiet") {
+		fmt.Println("...")
+	}
+
+	// get the main prompt response
+	promptResponse, err := createChatCompletion(
+		loadedResources.OpenAIClient,
+		loadedResources.ChatPersona,
+		loadedResources.InitialUserPrompt)
+	checkError(err, "could not complete request to openai", true)
+
+	// write response out to console
+	fmt.Println(promptResponse)
+
+	// log response to file
+	title := fetchTitleContent(loadedResources)
+	currentTime := time.Now().Local().Format("2006-01-02--15-04-05-MST")
+	logName := loadedResources.LogDir + currentTime + "." + title + ".md"
+	metaContent := "# " + title + "\n\n" + currentTime
+	content := metaContent + div("user") + loadedResources.InitialUserPrompt + div(loadedResources.ChatPersona.Name) + promptResponse + div("system") + loadedResources.ChatPersona.SystemMessage.Content
+	os.WriteFile(logName, []byte(content), 0644)
+}
+
+func chatMode() {
+	log.Fatal("chat mode is not implemented")
+}
+
+func configMode() {
+	log.Fatal("config mode is not implemented")
+}
+
+func showMode() {
+	log.Fatal("show mode is not implemented")
+}
+
+func fetchTitleContent(loadedResources LoadedResources) string {
+	titleContent := div("system") + loadedResources.ChatPersona.SystemMessage.Content + div("prompt") + loadedResources.InitialUserPrompt
+	title, err := createChatCompletion(
+		loadedResources.OpenAIClient,
+		loadedResources.TitlePersona,
+		titleContent)
+	checkError(err, "could not complete request to openai for title slug", false)
+	if title == "" {
+		return "unknown-topic"
+	} else {
+		return title
+	}
+}
+
+func loadConfig() LoadedResources {
+	// bind the config file
 	home, err := os.UserHomeDir()
-	checkFatal(err, "yoo depends on the home directory for configuration, and no home dir was found.")
-
+	checkError(err, "yoo depends on the home directory for configuration, and no home dir was found.", true)
 	config.AddDriver(yamlv3.Driver)
-	yooConfig := YooConfig{}
-	configFile := home + "/.config/yoo/config.yml"
+	configDir := home + "/.config/yoo/"
+	logDir := home + "/.yoo/"
+	configFile := configDir + "config.yml"
 	err = config.LoadFiles(configFile)
-	checkFatal(err, "yoo depends on '"+configFile+"' and it was not found.")
+	checkError(err, "yoo depends on '"+configFile+"' and it was not found.", true)
+	yooConfig := YooConfig{}
 	err = config.BindStruct("", &yooConfig)
-	checkFatal(err, "yoo had a problem parsing config from '"+configFile+"'")
+	checkError(err, "yoo had a problem parsing config from '"+configFile+"'", true)
 
+	// load the cli flags
 	flags := []string{"persona", "prompt", "quiet:bool", "no-log:bool"}
 	err = config.LoadFlags(flags)
-	checkWarn(err, "there was an issue parsing flags, so they will be ignored.")
+	checkError(err, "there was an issue parsing flags, so they will be ignored.", false)
 
-	// default to archie gpt-4 mainPersona
-	mainPersona := Persona{
+	// set up personas
+	chatPersona := Persona{
 		Name:  "archie",
 		Model: "gpt-4",
 	}
-
-	// default title persona
 	titlePersona := Persona{
-		Name:  "summer-slug",
-		Model: "gpt-3.5-turbo",
+		Name:  "slugger",
+		Model: "gpt-4",
 	}
 
-	// override with configured default persona, if exists
+	// start with configured default persona
 	for _, p := range yooConfig.Personas {
 		if p.Name == yooConfig.DefaultPersona {
-			mainPersona = p
+			chatPersona = p
 		}
 		if p.Name == yooConfig.TitlePersona {
 			titlePersona = p
@@ -76,63 +180,61 @@ func main() {
 	if personaFlag != "" {
 		for _, p := range yooConfig.Personas {
 			if p.Name == personaFlag {
-				mainPersona = p
+				chatPersona = p
 			}
 		}
 	}
 
-	// read the systemPrompt prompt
-	systemPrompt, systemFile, err := loadSystemPrompt(home, mainPersona)
-	checkFatal(err, "could not read the configured system prompt '"+systemFile+"'.")
+	// read the chat system prompt
+	chatSystemPrompt, chatSystemFile, err := loadSystemPrompt(home, chatPersona)
+	checkError(err, "could not read the configured system prompt '"+chatSystemFile+"'.", true)
 
-	// start userPrompt with cli argument
-	userPrompt := ""
-	if len(os.Args) == 2 {
-		userPrompt += os.Args[1]
-	} else if len(os.Args) > 2 {
-		userPrompt += config.String("prompt")
-	}
-
-	// if stdin was provided, add that to prompt
-	if !termutil.Isatty(os.Stdin.Fd()) {
-		inreader := bufio.NewReader(os.Stdin)
-		pipedinput, err := ioutil.ReadAll(inreader)
-		if err == nil {
-			userPrompt += "\n\n"
-			userPrompt += string(pipedinput)
-		}
-	}
-
-	// print something for ux
-	if !config.Bool("quiet") {
-		fmt.Println("...")
-	}
+	// read the title system prompt
+	titleSystemPrompt, titleSystemFile, err := loadSystemPrompt(home, titlePersona)
+	checkError(err, "could not read the configured title prompt '"+titleSystemFile+"'.", true)
 
 	// set up an openai client
-	client := openai.NewClient(yooConfig.Secrets.OpenAIKey)
+	openAIClient := openai.NewClient(yooConfig.Secrets.OpenAIKey)
 
-	// get the main prompt response
-	promptResponse, err := createChatCompletion(client, mainPersona.Model, systemPrompt, userPrompt)
-	checkFatal(err, "could not complete request to openai")
-
-	// write response out to console
-	fmt.Println(promptResponse)
-
-	// write response out to log
-	if !config.Bool("no-log") {
-		mainContent := div("prompt") + userPrompt + div("response") + promptResponse + div("system") + systemPrompt
-		titleSystemPrompt, titleSystemPromptFile, err := loadSystemPrompt(home, titlePersona)
-		checkFatal(err, "could not read the configured title system prompt '"+titleSystemPromptFile+"'.")
-		title, err := createChatCompletion(client, titlePersona.Model, titleSystemPrompt, mainContent)
-		checkWarn(err, "could not complete request to openai for title slug")
-		if err != nil {
-			title = "unknown-topic"
+	// determine mode
+	selectedMode := Normal // just `yoo`, or `yoo "what is..`
+	initialUserPrompt := ""
+	if len(os.Args) > 1 {
+		if os.Args[1] == "chat" { // `yoo chat`
+			selectedMode = Chat
+		} else if os.Args[1] == "config" { // `yoo config`
+			selectedMode = Config
+		} else if os.Args[1] == "show" { // `yoo show`
+			selectedMode = Show
 		}
-		currentTime := time.Now().Local().Format("2006-01-02--15-04-05-MST")
-		logName := home + "/.yoo/" + currentTime + "." + title + ".md"
-		metaContent := "# " + title + "\n\n" + currentTime
-		content := metaContent + mainContent
-		os.WriteFile(logName, []byte(content), 0644)
+		if selectedMode == Normal && os.Args[1][0] != '-' {
+			initialUserPrompt = os.Args[1]
+		}
+	}
+
+	// compose and return usable config
+	return LoadedResources{
+		OpenAIClient: openAIClient,
+		ChatPersona: LoadedPersona{
+			Name:  chatPersona.Name,
+			Model: chatPersona.Model,
+			SystemMessage: openai.ChatCompletionMessage{
+				Role:    openai.ChatMessageRoleSystem,
+				Content: chatSystemPrompt,
+			},
+		},
+		TitlePersona: LoadedPersona{
+			Name:  titlePersona.Name,
+			Model: titlePersona.Model,
+			SystemMessage: openai.ChatCompletionMessage{
+				Role:    openai.ChatMessageRoleSystem,
+				Content: titleSystemPrompt,
+			},
+		},
+		SelectedMode:      selectedMode,
+		InitialUserPrompt: initialUserPrompt,
+		ConfigDir:         configDir,
+		LogDir:            logDir,
 	}
 }
 
@@ -140,30 +242,24 @@ func div(title string) string {
 	return "\n\n## " + title + "\n\n"
 }
 
-func checkFatal(err error, message string) {
+func checkError(err error, message string, isFatal bool) {
 	if err != nil {
 		fmt.Println(message)
-		log.Fatal(err)
+		if isFatal {
+			log.Fatal(err)
+		} else {
+			fmt.Println(err)
+		}
 	}
 }
 
-func checkWarn(err error, message string) {
-	if err != nil {
-		fmt.Println(message)
-		fmt.Println(err)
-	}
-}
-
-func createChatCompletion(client *openai.Client, model string, system string, prompt string) (string, error) {
+func createChatCompletion(client *openai.Client, persona LoadedPersona, prompt string) (string, error) {
 	resp, err := client.CreateChatCompletion(
 		context.Background(),
 		openai.ChatCompletionRequest{
-			Model: model,
+			Model: persona.Model,
 			Messages: []openai.ChatCompletionMessage{
-				{
-					Role:    openai.ChatMessageRoleSystem,
-					Content: system,
-				},
+				persona.SystemMessage,
 				{
 					Role:    openai.ChatMessageRoleUser,
 					Content: prompt,
